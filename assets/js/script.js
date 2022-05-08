579f722ef08fc52483074c972934534d3b199dac
Forever Young <dy9040@gmail.com>
2022-05-08 18:36:18 -0500

    attached js script and built our skeleton for main page

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// 1 You will only have <span> 30 seconds</span> per Question 
+// 2. once you select your answer, you cant reselect.
+// 3. you cant select any option once time goes off.
+// 4. You cant exit from the Quiz while youre playing.
+// 5. You'll get points on the basis of your correct answers. 
+// 6. You'll be able to log high score. 
